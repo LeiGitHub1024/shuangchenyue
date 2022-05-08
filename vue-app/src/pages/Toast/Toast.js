241e40c9b0138287c98397572f9e5f4f643b547e
LeiGitHub1024 <cmcc.coder@gmail.com>
2022-05-08 17:16:36 +0800

    1

--- vue-app/src/pages/Toast/Toast.js
+++ vue-app/src/pages/Toast/Toast.js
@@ -8,7 +8,7 @@ const Toast = res => {
       content:res,
       remove() {  messageNode.remove() }
     })
-  app.vm = app.mount(messageNode)
+  app.vm = app.mount(messageNode) //把新建的div挂载（绑定？）至我们的Toast组件。
   document.body.appendChild(messageNode)   
 }
 export default Toast
